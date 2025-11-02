99afbaa8d980f82d071062e815d3542f8050e41a
Uomocosa <maggiori.samuele@gmail.com>
2025-11-02 13:05:26 +0100

    cleaned

--- new_import_system/new_import_system/__init__.py
+++ new_import_system/new_import_system/__init__.py
@@ -94,22 +94,6 @@ class CallableLoader(importlib.abc.Loader):
 
 
 
-# Works fine, but is a little hacky
-# def get_importer_filepath() -> Path:
-#     """
-#     Walks the stack to find the first frame that 
-#     is NOT part of the importlib machinery.
-#     """
-#     import_frames = inspect.stack()
-#     import_frames = [x for x in import_frames if not x.filename.startswith("<frozen importlib")]
-#     import_frames = [x for x in import_frames if not Path(x.filename) == THIS_FILE_PATH]
-#     if DEBUG and VERBOSE: [print(frame.filename) for frame in import_frames]
-#     if not import_frames: return None
-#     return P(import_frames[0].filename)
-
-
-
-
 def install_hook():
     """Prepends our custom finder to the meta_path."""
     if DEBUG: print(">>> new_import_system's hook installed")
--- new_import_system_tests/0_simple_package/package/test_from_inside.py
+++ new_import_system_tests/0_simple_package/package/test_from_inside.py
@@ -1,11 +1,3 @@
-# def test_get_importer_filepath():
-#     import new_import_system
-#     from pathlib import Path
-#     assert new_import_system.get_importer_filepath() == Path(__file__).absolute()
-#     # print(f">>> Path(__file__).resolve(): {Path(__file__).resolve()}")
-#     # print(f">>> Path(__file__).absolute(): {Path(__file__).absolute()}")
-#     # assert Path(__file__).resolve() == Path(__file__).absolute()
-
 def test_imports():
     print()
     import new_import_system
@@ -15,29 +7,3 @@ def test_imports():
     print(f"dir(package): {dir(package)}")
     # print(f"dir(package.submodule1): {dir(package.submodule1)}")
     # print(f"dir(package.submodule1.fun1): {dir(package.submodule1.fun1)}")
-
-
-# def test_1():
-#     import new_import_system
-#     import submodule1
-#     assert submodule1.fun1() == 'fun1'
-
-# def test_2():
-#     from submodule1 import fun1
-#     assert fun1() == 'fun1'
-
-# def test_3():
-#     from package import submodule1
-#     assert submodule1.fun1() == 'fun1'
-
-# def test_4():
-#     from package.submodule1 import fun1
-#     assert fun1() == 'fun1'
-
-# def test_5():
-#     from . import submodule1
-#     assert submodule1.fun1() == 'fun1'
-    
-# def test_6():
-#     from .submodule1 import fun1
-#     assert fun1() == 'fun1'
