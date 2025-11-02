e921c7e71cc5b32cd29cf03a8fe82c4a3f31aa36
Uomocosa <maggiori.samuele@gmail.com>
2025-11-02 10:19:00 +0100

    kinda working run_external tests

--- new_import_system/run_external_tests.py
+++ new_import_system/run_external_tests.py
@@ -5,17 +5,18 @@ from pathlib import Path
 
 # Find all immediate subdirectories in the 'tests' folder
 # that don't start with an underscore or dot.
-test_dir =  Path('.').absolute().parent / "new_import_system_tests"
+project_root = Path('.').absolute().parent
+test_dir =  project_root / "new_import_system_tests"
 dirs = [dir for dir in test_dir.iterdir() if dir.is_dir()]
 python_packages = [dir for dir in dirs if (dir / "__init__.py").exists()]
 # print(python_packages)
-relative_packages = [dir.relative_to(test_dir) for dir in python_packages]
+relative_packages = [dir.relative_to(project_root) for dir in python_packages]
 # print(relative_packages)
 
 run_test_dir = lambda dir: subprocess.run(
     [sys.executable, "-m", "pytest", str(dir)],
     text=True,
-    cwd=test_dir,
+    cwd=project_root,
 )
 
 results = [run_test_dir(dir) for dir in sorted(relative_packages)]
